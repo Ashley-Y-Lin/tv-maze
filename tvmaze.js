31d6a00d1b118bbe899effb6990142d94772632f
Ashley Lin <ashleyylin11@gmail.com>
2023-04-12 16:39:06 -0700

    make progress on displaying episodes

--- tvmaze.js
+++ tvmaze.js
@@ -1,10 +1,11 @@
 "use strict";
 
+/** declare const for DOM elements and API links */
 const $showsList = $("#showsList");
 const $episodesArea = $("#episodesArea");
 const $searchForm = $("#searchForm");
 
-const TVMAZE_SINGLE_SEARCH_ENDPOINT = "https://api.tvmaze.com/singlesearch/shows";
+const TVMAZE_SHOW_SEARCH_ENDPOINT = "https://api.tvmaze.com/search/shows";
 const DEFAULT_IMAGE = "https://tinyurl.com/tv-missing";
 
 /** Given a search term, search for tv shows that match that query.
@@ -13,39 +14,30 @@ const DEFAULT_IMAGE = "https://tinyurl.com/tv-missing";
  *    Each show object should contain exactly: {id, name, summary, image}
  *    (if no image URL given by API, put in a default image URL)
  */
-
 async function getShowsByTerm(term) {
   // ADD: Remove placeholder & make request to TVMaze search shows API.
-  const showInfo = await axios.get(TVMAZE_SINGLE_SEARCH_ENDPOINT, {
+  const showInfo = await axios.get(TVMAZE_SHOW_SEARCH_ENDPOINT, {
     params: {
       q: term,
     },
   });
 
-  console.log('showInfo response = ', showInfo);
+  console.log('showInfo data = ', showInfo.data);
 
-  const showId = showInfo.data.id;
+  let allShows = [];
 
-  const episodes = await axios.get(`http://api.tvmaze.com/shows/${showId}/episodes`, {
-  });
-
-  console.log('episodes response = ', episodes);
-
-  let showObjects = [];
-
-  for (let show of episodes.data) {
+  for (let show of showInfo.data) {
     let showCurr = {
-      id: show.id,
-      name: show.name,
-      summary: show.summary
+      id: show.show.id,
+      name: show.show.name,
+      summary: show.show.summary
     };
-    let showImageUrl = show.image.original || DEFAULT_IMAGE;
+    let showImageUrl = show.show.image.original || DEFAULT_IMAGE;
     showCurr.image = showImageUrl;
-    showObjects.push(showCurr);
+    allShows.push(showCurr);
   }
 
-  console.log('showObjects', showObjects)
-  return showObjects;
+  return allShows;
 }
 
 
@@ -53,7 +45,6 @@ async function getShowsByTerm(term) {
  *
  * A show is {id, name, summary, image}
  * */
-
 function displayShows(shows) {
   $showsList.empty();
 
@@ -84,7 +75,6 @@ function displayShows(shows) {
 /** Handle search form submission: get shows from API and display.
  *    Hide episodes area (that only gets shown if they ask for episodes)
  */
-
 async function searchShowsAndDisplay() {
   const term = $("#searchForm-term").val();
   const shows = await getShowsByTerm(term);
@@ -93,6 +83,7 @@ async function searchShowsAndDisplay() {
   displayShows(shows);
 }
 
+/** Add event listener to submit button */
 $searchForm.on("submit", async function handleSearchForm(evt) {
   evt.preventDefault();
   await searchShowsAndDisplay();
@@ -103,10 +94,56 @@ $searchForm.on("submit", async function handleSearchForm(evt) {
  *      { id, name, season, number }
  */
 
-// async function getEpisodesOfShow(id) { }
+async function getEpisodesOfShow(id) {
+  const episodes = await axios.get(`http://api.tvmaze.com/shows/${id}/episodes`, {
+  });
+
+  console.log('episodes response = ', episodes);
+
+  let allEpisodes = [];
+
+  for (let show of episodes.data) {
+    let showCurr = {
+      id: show.id,
+      name: show.name,
+      season: show.season,
+      number: show.number
+    };
+    let showImageUrl = show.image.original || DEFAULT_IMAGE;
+    showCurr.image = showImageUrl;
+    allEpisodes.push(showCurr);
+  }
+
+  console.log('allEpisodes', allEpisodes);
+  return allEpisodes;
+}
 
 /** Write a clear docstring for this function... */
 
-// function displayEpisodes(episodes) { }
+function displayEpisodes(episodes) {
+  for (let episode of episodes) {
+    let singleEpisode = $("<li>", {
+      text: `${episode.name} (season ${episode.season}, number ${episode.number})`
+    });
+    $("#episodesList").append(singleEpisode);
+  }
+  $("#episodesArea").attr('style', 'display: inline');
+}
 
 // add other functions that will be useful / match our structure & design
+
+function getAndDisplayEpisodes(id) {
+  console.log("id", id)
+  const episodes = getEpisodesOfShow(id);
+  displayEpisodes(episodes);
+}
+
+$("#showsList").on("click", ".Show-getEpisodes", function (event) {
+  const targetShowDiv = event.target.closest(".Show");
+  console.log("target show div", targetShowDiv)
+
+  const targetShowID = targetShowDiv.attr("data-value")
+  console.log("target show id", targetShowID)
+
+  getAndDisplayEpisodes(targetShowID);
+});
