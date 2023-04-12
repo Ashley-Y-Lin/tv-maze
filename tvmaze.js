a234f05402c56c03defe0ad5e718db7d7d5f3ecc
Ashley Lin <ashleyylin11@gmail.com>
2023-04-12 12:17:25 -0700

    add getShowsByTerm

--- tvmaze.js
+++ tvmaze.js
@@ -4,6 +4,8 @@ const $showsList = $("#showsList");
 const $episodesArea = $("#episodesArea");
 const $searchForm = $("#searchForm");
 
+const TVMAZE_SINGLE_SEARCH_ENDPOINT = "https://api.tvmaze.com/singlesearch/shows";
+const DEFAULT_IMAGE = "https://images.unsplash.com/photo-1615751072497-5f5169febe17?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y3V0ZSUyMGRvZ3xlbnwwfHwwfHw%3D&w=1000&q=80";
 
 /** Given a search term, search for tv shows that match that query.
  *
@@ -12,27 +14,56 @@ const $searchForm = $("#searchForm");
  *    (if no image URL given by API, put in a default image URL)
  */
 
-async function getShowsByTerm( /* term */) {
+async function getShowsByTerm(term) {
   // ADD: Remove placeholder & make request to TVMaze search shows API.
+  const showInfo = await axios.get(TVMAZE_SINGLE_SEARCH_ENDPOINT, {
+    params: {
+      q: term,
+    },
+  });
 
-  return [
+  console.log('showInfo response = ', showInfo);
+
+  const showId = showInfo.data.id;
+
+  const episodes = await axios.get(`http://api.tvmaze.com/shows/${showId}/episodes`, {
+  });
+
+  console.log('episodes response = ', episodes);
+
+  let showObjects = [];
+
+  for (let show of episodes.data) {
+    let showCurr = {
+      id: show.id,
+      name: show.name,
+      summary: show.summary
+    };
+    let showImageUrl = show.image.original || DEFAULT_IMAGE;
+    showCurr.image = showImageUrl;
+    showObjects.push(showCurr);
+  }
+
+  console.log('showObjects', showObjects)
+
+  /*return [
     {
       id: 1767,
       name: "The Bletchley Circle",
       summary:
-        `<p><b>The Bletchley Circle</b> follows the journey of four ordinary 
+        `<p><b>The Bletchley Circle</b> follows the journey of four ordinary
            women with extraordinary skills that helped to end World War II.</p>
-         <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their 
-           normal lives, modestly setting aside the part they played in 
-           producing crucial intelligence, which helped the Allies to victory 
+         <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their
+           normal lives, modestly setting aside the part they played in
+           producing crucial intelligence, which helped the Allies to victory
            and shortened the war. When Susan discovers a hidden code behind an
-           unsolved murder she is met by skepticism from the police. She 
+           unsolved murder she is met by skepticism from the police. She
            quickly realises she can only begin to crack the murders and bring
            the culprit to justice with her former friends.</p>`,
       image:
           "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
     }
-  ]
+  ]*/
 }
 
 
@@ -48,9 +79,9 @@ function displayShows(shows) {
     const $show = $(`
         <div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
          <div class="media">
-           <img 
-              src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg" 
-              alt="Bletchly Circle San Francisco" 
+           <img
+              src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg"
+              alt="Bletchly Circle San Francisco"
               class="w-25 me-3">
            <div class="media-body">
              <h5 class="text-primary">${show.name}</h5>
@@ -80,7 +111,7 @@ async function searchShowsAndDisplay() {
   displayShows(shows);
 }
 
-$searchForm.on("submit", async function handleSearchForm (evt) {
+$searchForm.on("submit", async function handleSearchForm(evt) {
   evt.preventDefault();
   await searchShowsAndDisplay();
 });
